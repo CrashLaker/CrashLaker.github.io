0d895aba0f2ad80a5442fd8fe2b5060c23c8fa98
carlos <carlos.tomigawa@btgpactual.com>
2020-05-03 12:42:48 -0300

    sync2

---
+++ assets/static/codimd/config.js
@@ -0,0 +1,13 @@
+window.domain = ''
+window.urlpath = ''
+window.debug = false
+window.version = '1.4.1'
+window.plantumlServer = 'https://www.plantuml.com/plantuml'
+
+window.allowedUploadMimeTypes = ["image/jpeg","image/png","image/jpg","image/gif","image/svg+xml"]
+
+window.defaultUseHardbreak = true
+
+window.linkifyHeaderStyle = 'keep-case'
+
+window.DROPBOX_APP_KEY = ''
