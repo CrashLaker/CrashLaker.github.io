63e26cba413ab7ead486b66210c818e8421ec5fd
CrashLaker <crashlaker@gmail.com>
2021-01-22 16:06:45 -0500

    sync2

--- start-dev.sh
+++ start-dev.sh
@@ -2,17 +2,19 @@
 
 
 img="crashlaker-jekyll-dev"
-port=4000
+port_s=4000
+port_l=4001
 
 docker rm -f ${img}
 docker run -dit \
     --name ${img} \
     -v `pwd`:/folder \
-    -p ${port}:4000 \
+    -p ${port_s}:4000 \
+    -p ${port_l}:4001 \
     jekyll/jekyll bash -c "
     cd /folder
     bundle install
-    jekyll serve -l
+    jekyll serve -l --livereload_port 4001
     "
 
 
