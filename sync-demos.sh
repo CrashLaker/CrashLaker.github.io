85cb82341e8061b58104685ee3a9c4fdf8e8bff9
CrashLaker <crashlaker@gmail.com>
2021-02-05 09:01:46 -0500

    sync2

--- sync-demos.sh
+++ sync-demos.sh
@@ -4,4 +4,7 @@ folder_from=/root/codeserver_playground/horizon-demo/dist/
 folder_to=horizon-demo
 rsync -avz --delete codeserver:${folder_from} ${folder_to}
 
+folder_from=/root/codeserver_vuepress-grafana-horizon/grafana-horizon/grafana-horizon/
+folder_to=grafana-horizon
+rsync -avz --delete codeserver:${folder_from} ${folder_to}
 
